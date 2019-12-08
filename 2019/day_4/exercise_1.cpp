92c1f7bf4946f61efbbee1c28ffe1fc2d99f0874
Erik Vroon <erik.vroon22@gmail.com>
2019-12-08 18:20:07 +0100

    Format c++ code properly

--- 2019/day_4/exercise_1.cpp
+++ 2019/day_4/exercise_1.cpp
@@ -4,26 +4,29 @@
 
 int main()
 {
-	std::ifstream myfile("input.txt");
-	std::string line;
+    std::ifstream myfile("input.txt");
+    std::string line;
     std::getline(myfile, line);
 
     std::string token = line.substr(0, line.find("-"));
-	int start = std::stoi(line.substr(0, line.find("-")));
-	int end = std::stoi(line.substr(line.find("-") + 1));
+    int start = std::stoi(line.substr(0, line.find("-")));
+    int end = std::stoi(line.substr(line.find("-") + 1));
 
     int result = 0;
 
-	for (int i = start; i <= end; i++) {
+    for (int i = start; i <= end; i++)
+    {
         std::string s = std::to_string(i);
         int last = 0;
         bool found = false;
         bool failed = false;
 
-        for (int j = 0; j < s.length(); j++) {
+        for (int j = 0; j < s.length(); j++)
+        {
             int current = s[j] - 48;
 
-            if (current < last) {
+            if (current < last)
+            {
                 failed = true;
                 break;
             }
@@ -35,8 +38,8 @@ int main()
         }
         if (!failed && found)
             result += 1;
-	}
+    }
 
     std::cout << result << std::endl;
-	return 0;
+    return 0;
 }
--- 2019/day_4/exercise_2.cpp
+++ 2019/day_4/exercise_2.cpp
@@ -4,45 +4,47 @@
 
 int main()
 {
-	std::ifstream myfile("input.txt");
-	std::string line;
+    std::ifstream myfile("input.txt");
+    std::string line;
     std::getline(myfile, line);
 
     std::string token = line.substr(0, line.find("-"));
-	int start = std::stoi(line.substr(0, line.find("-")));
-	int end = std::stoi(line.substr(line.find("-") + 1));
+    int start = std::stoi(line.substr(0, line.find("-")));
+    int end = std::stoi(line.substr(line.find("-") + 1));
 
     int result = 0;
 
-	for (int i = start; i <= end; i++) {
+    for (int i = start; i <= end; i++)
+    {
         std::string s = std::to_string(i);
         int last = 0;
         int lastlast = 0;
         bool found = false;
         bool failed = false;
 
-        for (int j = 0; j < s.size(); j++) {
+        for (int j = 0; j < s.size(); j++)
+        {
             int current = s[j] - 48;
             int next = s[j + 1] - 48;
-            
+
             if (j > 1)
                 lastlast = s[j - 2] - 48;
 
-            if (current < last) {
+            if (current < last)
+            {
                 failed = true;
                 break;
             }
 
-            if (current == last && lastlast != current && next != current) {
+            if (current == last && lastlast != current && next != current)
                 found = true;
-            }
 
             last = current;
         }
         if (!failed && found)
             result += 1;
-	}
+    }
 
     std::cout << result << std::endl;
-	return 0;
+    return 0;
 }
