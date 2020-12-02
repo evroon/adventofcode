cad5143b49c8d6e90145e8b7f37854645bdf5843
Erik Vroon <erik.vroon22@gmail.com>
2020-12-02 19:41:14 +0100

    Improve code readability

--- 2020/lib.cpp
+++ 2020/lib.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
+#include <chrono>
 
 #include "day01.cpp"
 #include "day02.cpp"
@@ -8,24 +9,29 @@
 int run_day(uint day)
 {
     std::ifstream input;
+    int (*function)(std::ifstream &input) = nullptr;
     int result = -1;
 
     switch (day)
     {
-    case 1:
-        input = std::ifstream("../inputs/day01.txt");
-        result = day1_part2(input);
-        break;
-
-    case 2:
-        input = std::ifstream("../inputs/day02.txt");
-        result = day2_part2(input);
-        break;
+    case  1: function = day1_part2; break;
+    case  2: function = day2_part2; break;
 
     default:
         std::cout << "Invalid day: " << day << std::endl;
-        break;
+        return -1;
     }
+
+    char path[96];
+    sprintf(path, "../inputs/day%02d.txt", day);
+    input = std::ifstream(path);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    result = function(input);
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    int duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+    std::cout << "Duration: " << duration << std::endl;
 
     return result;
 }
