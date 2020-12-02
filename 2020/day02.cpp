40b0fc125aba87435fa3315577ccc28d795b6ba9
Erik Vroon <erik.vroon22@gmail.com>
2020-12-02 09:56:14 +0100

    Add day 2

---
+++ 2020/day02.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <vector>
+
+int main()
+{
+    std::ifstream input("inputs/day02.txt");
+    std::string line;
+    int occurences = 0;
+
+    while (std::getline(input, line))
+    {
+        int min = std::atoi(line.substr(0, line.find("-")).c_str());
+        std::string rest = line.substr(line.find("-")+1, line.size() - 1);
+        int max = std::atoi(rest.substr(0, rest.find(" ")).c_str());
+        char c = rest.substr(rest.find(" ") + 1, rest.find(":") - 1)[0];
+
+        std::string password = line.substr(line.find(":") + 2, line.size() - 1);
+        if ((password[min-1] == c) != (password[max-1] == c)) {
+            occurences++;
+        }
+    }
+
+    std::cout << occurences << std::endl;
+
+    return 0;
+}
