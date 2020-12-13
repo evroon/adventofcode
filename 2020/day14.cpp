d86943bab9e552ee60d1a51a8b186afb72df7033
Erik Vroon <erik.vroon22@gmail.com>
2020-12-13 14:39:32 +0100

    Add templates for remaining days

---
+++ 2020/day14.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <vector>
+#include <string_view>
+#include <algorithm>
+#include <math.h>
+
+#include "manager.h"
+
+R day14_part2(std::ifstream& input)
+{
+    std::string line;
+    std::vector<int> list;
+    int result = 0;
+
+    while (std::getline(input, line))
+        list.push_back(atoi(line.c_str()));
+
+    return result;
+}
+
+ADD_SOLUTION(14)
