3c312e151f153a9e92029694cc34cfe04ab15cd5
Erik Vroon <erik.vroon22@gmail.com>
2021-10-29 20:25:24 +0200

    Add part 2 of day 3

--- 2016/aoc2016/day03.py
+++ 2016/aoc2016/day03.py
@@ -1,4 +1,15 @@
-from typing import Tuple
+from typing import List
+
+
+def check_triangle(triangle: List[int]) -> bool:
+    for i in range(3):
+        j = (i + 1) % 3
+        k = (i + 2) % 3
+
+        if triangle[j] + triangle[k] <= triangle[i]:
+            return False
+
+    return True
 
 
 def part1(input: str) -> int:
@@ -6,26 +17,39 @@ def part1(input: str) -> int:
     result = 0
 
     for triangle in triangles[:-1]:
-        side1 = int(triangle[:5].strip())
-        side2 = int(triangle[5:10].strip())
-        side3 = int(triangle[10:].strip())
-        sides = [side1, side2, side3]
-        success = True
+        sides = []
+        for i, j in enumerate(range(0, 3)):
+            sides.append(int(triangle[j*5:(j+1)*5].strip()))
 
-        for i in range(3):
-            j = (i + 1) % 3
-            k = (i + 2) % 3
-            print(i, j, k)
-
-            if sides[j] + sides[k] <= sides[i]:
-                success = False
-                break
-
-        if success:
+        if check_triangle(sides):
             result += 1
 
     return result
 
 
 def part2(input: str) -> int:
-    pass
+    triangles = input.split('\n')
+    result = 0
+
+    for r in range(len(triangles) // 3):
+        rows: List[str] = [
+            triangles[r*3],
+            triangles[r*3+1],
+            triangles[r*3+2],
+        ]
+
+        triangle_set = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]
+
+        for t in range(3):
+            for j in range(3):
+                triangle_set[t][j] = int(rows[j][t*5:(t+1)*5].strip())
+
+        for t in triangle_set:
+            if check_triangle(t):
+                result += 1
+
+    return result
--- 2016/tests/test_days.py
+++ 2016/tests/test_days.py
@@ -6,20 +6,17 @@ from aoc2016.run import run_day
 outcomes = {
     1: [242, 150],
     2: ['84452', 'D65C3'],
-    3: [869, None]
+    3: [869, 1544]
 }
 
 
 @pytest.mark.parametrize(
     'day,is_part_two',
     itertools.product(
-        range(1, 32),
+        outcomes.keys(),
         [False, True]
     )
 )
 def test_day(day: int, is_part_two: bool) -> None:
-    if day not in outcomes:
-        return
-
     expected = outcomes[day][1 if is_part_two else 0]
     assert expected == run_day(day, is_part_two)
