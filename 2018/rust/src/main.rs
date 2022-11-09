e3bc57101c136cc5fe12ed710d84e42cc9c668c6
Erik Vroon <erik.vroon@channable.com>
2022-11-09 18:47:30 +0100

    fixup! fixup! Create 2018 rust setup

--- 2018/rust/src/main.rs
+++ 2018/rust/src/main.rs
@@ -1,61 +1,53 @@
-
 extern crate aoc_2018;
 extern crate chrono;
-#[macro_use]
-extern crate clap;
 
 use std::fs;
 use std::io;
 use std::time::Instant;
 
-use clap::Arg;
-
 use aoc_2018::get_impl;
+use clap::Parser;
+
+/// Run AoC solutions
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Number of the day to execute
+    #[arg(short, long)]
+    day: u32,
+
+    /// Run part 2 instead of part 1
+    #[arg(short, long)]
+    part2: bool,
+
+    /// Use stdin input instead of file
+    #[arg(short, long)]
+    stdin: bool,
+
+    /// Print the time for the result
+    #[arg(short, long, default_value = "true")]
+    time: bool,
+}
 
 fn main() {
-    let matches = app_from_crate!()
-        .arg(
-            Arg::with_name("day")
-                .value_name("DAY")
-                .help("Number of the day to execute")
-                .required(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("part2")
-                .short("2")
-                .help("Run part 2 instead of part 1")
-                .long("part2"),
-        )
-        .arg(
-            Arg::with_name("input")
-                .short("i")
-                .long("input")
-                .help("Optional input file, stdin otherwise")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("time")
-                .short("t")
-                .long("time")
-                .help("Print the time for the result"),
-        )
-        .get_matches();
+    let args = Args::parse();
+    println!("inputs/{:0>2}.txt", args.day);
 
-    let mut implementation = get_impl(value_t_or_exit!(matches, "day", u32));
-    let mut data: Box<dyn io::Read> = match matches.value_of("input") {
-        Some(filename) => Box::new(fs::File::open(filename).unwrap()),
-        None => Box::new(io::stdin()),
+    let mut implementation = get_impl(args.day);
+    let mut data: Box<dyn io::Read> = match args.stdin {
+        true => Box::new(io::stdin()),
+        false => Box::new(fs::File::open(format!("inputs/{:0>2}.txt", args.day)).unwrap()),
     };
 
     let begin = Instant::now();
-    let result = if matches.is_present("part2") {
-        implementation.part2(&mut data)
-    } else {
-        implementation.part1(&mut data)
+    let result = match args.part2 {
+        true => implementation.part2(&mut data),
+        false => implementation.part1(&mut data),
     };
-    if matches.is_present("time") {
+
+    if args.time {
         eprintln!("Duration: {:?}", Instant::now().duration_since(begin));
     }
+
     println!("{}", result);
 }
