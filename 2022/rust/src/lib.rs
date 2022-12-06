2d01dd362fe1c5fac9efb2b885f2b5f88ff25959
Erik Vroon <erik.vroon@channable.com>
2022-12-06 22:10:12 +0100

    Fix rust setup 2022

--- 2022/rust/src/lib.rs
+++ 2022/rust/src/lib.rs
@@ -3,11 +3,9 @@ extern crate itertools;
 extern crate regex;
 
 pub mod common;
-pub mod day01;
 
 pub fn get_impl(day: u32) -> Box<dyn common::Solution> {
     match day {
-        1 => Box::new(day01::Day01::new()),
         val => panic!("Unimplemented day {}", val),
     }
 }
