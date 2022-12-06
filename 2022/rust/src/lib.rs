9a2da1e9746a2949d0b6820643f2e4d84cf70ba3
Erik Vroon <erik.vroon@channable.com>
2022-12-06 22:18:10 +0100

    Fix rust setup 2022

--- 2022/rust/src/lib.rs
+++ 2022/rust/src/lib.rs
@@ -6,6 +6,7 @@ pub mod common;
 
 pub fn get_impl(day: u32) -> Box<dyn common::Solution> {
     match day {
+        1 => panic!("Unimplemented day 1"),
         val => panic!("Unimplemented day {}", val),
     }
 }
